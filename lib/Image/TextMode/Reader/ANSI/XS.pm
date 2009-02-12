38d9516ebef7b76038275de8f93a7ab6a657e15b
Brian Cassidy <bricas@cpan.org>
2009-02-12 18:35:37 +0000

    adding benchmark data

--- lib/Image/TextMode/Reader/ANSI/XS.pm
+++ lib/Image/TextMode/Reader/ANSI/XS.pm
@@ -28,6 +28,15 @@ Image::TextMode::Reader::ANSI::XS - Fast ANSI image parsing
 
 =head1 BENCHMARK
 
+Parsing 74K of ANSI 25 times yields:
+
+    Benchmark: timing 25 iterations of PP, XS...
+            PP: 24 wallclock secs (23.84 usr +  0.00 sys = 23.84 CPU) @  1.05/s (n=25)
+            XS:  1 wallclock secs ( 1.00 usr +  0.00 sys =  1.00 CPU) @ 25.00/s (n=25)
+         Rate    PP    XS
+    PP 1.05/s    --  -96%
+    XS 25.0/s 2284%    --
+
 =head1 AUTHOR
 
 Brian Cassidy E<lt>bricas@cpan.orgE<gt>
