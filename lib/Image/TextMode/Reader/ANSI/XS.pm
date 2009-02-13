4c766cae8bdbdc0a00537ad29376cd8890b5153c
Brian Cassidy <bricas@cpan.org>
2009-02-13 15:52:04 +0000

    added docs. updated changes.

--- lib/Image/TextMode/Reader/ANSI/XS.pm
+++ lib/Image/TextMode/Reader/ANSI/XS.pm
@@ -15,7 +15,17 @@ Image::TextMode::Reader::ANSI::XS - Fast ANSI image parsing
 
 =head1 SYNOPSIS
 
+    # The XS reader will automatically be used instead of the pure-perl version
+    my $ansi = Image::TextMode::Format::ANSI->new
+    $ansi->read( shift );
+
 =head1 DESCRIPTION
+
+To parse an ANSI file, we use a simple state machine and examine each character
+individually. This proves to be a little on the slow side in pure-perl form.
+
+This module endeavors to re-implement the parsing in XS/C. The results show
+a major speed increase; about 25 times faster.
 
 =head1 INSTALLATION
 
@@ -25,6 +35,11 @@ Image::TextMode::Reader::ANSI::XS - Fast ANSI image parsing
 	make install
 
 =head1 METHODS
+
+=head2 _read( $image, $fh, \%options )
+
+This is an XS-based version of L<Image::TextMode::Reader::ANSI>'s method of the
+same name.
 
 =head1 BENCHMARK
 
